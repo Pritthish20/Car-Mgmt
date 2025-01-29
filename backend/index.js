1d81db1d4eb563353fc14689097ebf2ee09d9f34
obsideen <97191699+Pritthish20@users.noreply.github.com>
2025-01-30 04:28:57 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -29,17 +29,7 @@ app.use(cors({
       origin: ["http://localhost:5173","https://car-mgmt-2kse.vercel.app/"], 
       credentials: true
     }));
-    
-
-    app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-      if (req.method === "OPTIONS") {
-          return res.sendStatus(200);
-      }
-      next();
-  });
+ 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
