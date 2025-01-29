ae4be0910bfd9f91f3920486ca587048bb9adcb2
obsideen <97191699+Pritthish20@users.noreply.github.com>
2025-01-30 04:05:54 +0530

    updated index

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,17 @@ app.use(cors({
       origin: "http://localhost:5173", 
       credentials: true
     }));
+    
+
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      if (req.method === "OPTIONS") {
+          return res.sendStatus(200);
+      }
+      next();
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
