02096c0ca9055878acf1bfb8b5044b3fa742c5c3
obsideen <97191699+Pritthish20@users.noreply.github.com>
2025-01-30 13:39:11 +0530

    cors 3

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,15 +1,11 @@
 import { fetchBaseQuery,createApi } from "@reduxjs/toolkit/query/react";
 import {BASE_URL} from '../constants'
 
-<<<<<<< HEAD
+
 const baseQuery=fetchBaseQuery({
     baseUrl: BASE_URL ,
      credentials:"include",
     });
-=======
-const baseQuery=fetchBaseQuery({baseUrl: BASE_URL });
->>>>>>> cde62efa4c428218d0c581529393dcf34cb76523
-
 export const apiSlice=createApi({
     baseQuery,
     endpoints:()=>({}),
